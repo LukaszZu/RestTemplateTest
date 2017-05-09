92bc7cf0ee171eb3177626ab7d7593aeeb4cf6f2
lz.zukowski@gmail.com <lz.zukowski@gmail.com>
2017-05-09 23:10:06 +0200

    Refactor PrefixSkip method

--- src/main/java/com/zz/MvcBoot.java
+++ src/main/java/com/zz/MvcBoot.java
@@ -5,19 +5,22 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.function.Function;
 
 /**
  * Created by zulk on 21.11.16.
@@ -44,11 +47,13 @@ public class MvcBoot {
         }
 
         public TestClass getTest2() {
-//            ResponseEntity<String> forEntity = restTemplate.getForEntity("/test2", String.class);
-//            String body = forEntity.getBody();
-//            System.out.println(body);
             return restTemplate.getForObject("/test2", TestClass.class);
         }
+
+        public TestClass[] getTest3() {
+            return restTemplate.getForObject("/test3", TestClass[].class);
+        }
+
     }
 
     @Configuration
@@ -56,40 +61,70 @@ public class MvcBoot {
 
         @Bean
         public ObjectMapper mapper() {
-            ObjectMapper om = new ObjectMapper(new MyMapperFactory());
+            Function<InputStream, InputStream> f = x -> new PrefixSkipInputStream(x);
+
+            ObjectMapper om = new ObjectMapper(new InputModificationJsonFactory(f));
             om.enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES);
             return om;
         }
     }
 
 
-    class MyStream extends FilterInputStream {
-        String pattern = "})),\n";
+    class PrefixSkipInputStream extends FilterInputStream {
+        public static final String DEFAULT_PREFIX = ")]}', ";
+        public static final String GUAM_PREFIX = ")]}',\n";
 
-        protected MyStream(InputStream inputStream) {
+        private boolean prefixHandled = false;
+        private String pattern = DEFAULT_PREFIX;
+
+        public PrefixSkipInputStream(InputStream inputStream, String pattern) {
+            super(inputStream);
+            Assert.notNull(pattern, "Pattern cannot be null");
+            this.pattern = pattern;
+        }
+
+        protected PrefixSkipInputStream(InputStream inputStream) {
             super(inputStream);
         }
 
         @Override
         public int read(byte[] bytes, int i, int i1) throws IOException {
-            byte[] p = pattern.getBytes();
-            int read1 = in.read(p);
-            if (read1 == pattern.length()) {
-                if (!Arrays.equals(p, pattern.getBytes())) {
-                    System.out.println("Prefix not matches");
-                    in.reset();
-                }
+            if (prefixHandled) {
+                return super.read(bytes, i, i1);
             }
+
+            byte[] patternBytes = pattern.getBytes();
+            int readBytes = in.read(patternBytes);
+
+            if (readBytes < pattern.length()) {
+                in.reset();
+                return super.read(bytes, i, i1);
+            }
+
+            prefixHandled = handlePrefix(patternBytes);
             return super.read(bytes, i, i1);
+        }
+
+        private boolean handlePrefix(byte[] p) throws IOException {
+            if (!Arrays.equals(p, pattern.getBytes())) {
+                in.reset();
+            }
+            return true;
         }
 
     }
 
-    class MyMapperFactory extends JsonFactory {
+    class InputModificationJsonFactory extends JsonFactory {
+
+        private Function<InputStream, InputStream> factory;
+
+        InputModificationJsonFactory(Function<InputStream, InputStream> functionalDecorators) {
+            this.factory = functionalDecorators;
+        }
+
         @Override
         public JsonParser createParser(InputStream in) throws IOException, JsonParseException {
-            System.out.println("PAAAA");
-            return super.createParser(new MyStream(in));
+            return super.createParser(factory.apply(in));
         }
     }
 
--- src/test/java/com/zz/TemplateTest.java
+++ src/test/java/com/zz/TemplateTest.java
@@ -1,19 +1,27 @@
 package com.zz;
 
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableList;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static com.zz.MvcBoot.PrefixSkipInputStream.DEFAULT_PREFIX;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.client.MockRestServiceServer;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
@@ -35,10 +43,18 @@ public class TemplateTest {
     private MvcBoot.ServiceClient service;
 
     String jsonString;
+    String bigJson;
 
     @Before
     public void before() throws IOException {
         jsonString = String.join("\n", Files.readAllLines(ctx.getResource("test.json").getFile().toPath()));
+
+        ObjectMapper mapper = Jackson2ObjectMapperBuilder.json().featuresToEnable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES).build();
+        TestClass testClass = mapper.readValue(jsonString, TestClass.class);
+        List<TestClass> testClassList = IntStream.range(0, 1000).boxed().map(f -> testClass).collect(Collectors.toList());
+
+        bigJson = mapper.writeValueAsString(testClassList);
+        System.out.println(bigJson);
     }
 
     @Test
@@ -58,12 +74,32 @@ public class TemplateTest {
 
     @Test
     public void prefixShouldBeRemoved() throws IOException {
-        server.expect(requestTo("/test2")).andRespond(withSuccess("})),\n" + jsonString, MediaType.APPLICATION_JSON));
-        TestClass test2 = service.getTest2();
-
-        System.out.println(test2);
+        server.expect(requestTo("/test2")).andRespond(withSuccess(DEFAULT_PREFIX + jsonString, MediaType.APPLICATION_JSON));
+        TestClass tc = service.getTest2();
+        //then
+        assertThat(tc.getTotal(), equalTo(123456));
+        assertThat(tc.getItems().size(), equalTo(2));
+        assertThat(tc.getItems(), contains(items()));
     }
-    
+
+    @Test
+    public void shouldRemovePrefixWhenBigInput() throws IOException {
+        server.expect(requestTo("/test3")).andRespond(withSuccess(DEFAULT_PREFIX + bigJson, MediaType.APPLICATION_JSON));
+        TestClass[] tc = service.getTest3();
+        //then
+        assertThat(tc.length, equalTo(1000));
+    }
+
+    @Test
+    public void shouldParseWhenBigInput() throws IOException {
+        server.expect(requestTo("/test3")).andRespond(withSuccess(bigJson, MediaType.APPLICATION_JSON));
+        TestClass[] tc = service.getTest3();
+        //then
+        assertThat(tc.length, equalTo(1000));
+    }
+
+
+
     private Item[] items() {
         return ImmutableList.of(new Item(111),new Item(222)).toArray(new Item[0]);
     }
